050cb70e3a037b960b2752657bf7f904d56f4d9f
Thiago <thiago.poison@gmail.com>
2023-02-02 17:17:31 -0300

    create pokemon, pokedex and paginationJS

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,17 +1,46 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Searchbar from "./components/Searchbar";
-import React from "react";
+import React, {useEffect, useState} from "react";
+import Pokedex  from "./components/Pokedex";
+import { getPokemons, getPokemonData } from "./api";
 
 function App() {
+
+    const [page, setPage] = useState(0);
+    const [totalPages, setTotalPages] = useState(0);
+    const [loading, setLoading] = useState(false);
+    const [pokemons, setPokemons] = useState( [] );
+
+  const itensPerPage = 25
+    const fetchPokemons = async () => {
+      try {
+        setLoading(true)
+        const data = await getPokemons(itensPerPage, itensPerPage * page);
+        const promises = data.results.map(async (pokemon) => {
+         return await getPokemonData(pokemon.url)
+        });
+
+        const results = await Promise.all(promises); 
+        setPokemons(results);
+        setLoading(false);
+        setTotalPages(Math.ceil(data.count / itensPerPage))
+      } catch (error) {
+        console.log("fetchPokemons error: ", error);
+      }
+    }
+
+    useEffect (() => {
+    console.log("carregou")
+    fetchPokemons();
+    }, [page] )
 
   return (
       
     <div>
       <Navbar />
-      <Searchbar 
-      />
-      <div className="App"></div>
+      <Searchbar />
+      <Pokedex pokemons={pokemons} loading={loading} page={page} setPage={setPage} totalPages={totalPages}/>
     </div>
   );
 }
